b155b236a14df63bf8ce7b56b6c809448375979a
Artem Vasiliev <artem.job@gmail.com>
2019-07-15 11:52:30 +0300

    chore(deps): upgrade deps

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 import test from 'ava';
 import m from '..';
 
-test(async t => {
+test('main', async t => {
   const data = await m({repoSlug: "artemv/circleci-step-outputter", buildNum: 2, step: 'test'});
   t.true(data[0].length > 0);
 });
